acd0764a87a816fd89968692dd8617b9b774b86f
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2023-02-01 18:18:05 +0200

    statistica nr 3

--- src/main/java/com/springmvc/SpringMVC/SpringDW/repositories/ProductDimRepository.java
+++ src/main/java/com/springmvc/SpringMVC/SpringDW/repositories/ProductDimRepository.java
@@ -2,6 +2,31 @@ package com.springmvc.SpringMVC.SpringDW.repositories;
 
 import com.springmvc.SpringMVC.SpringDW.models.ProductDim;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProductDimRepository extends JpaRepository<ProductDim, Integer> {
+
+
+    @Query(value = "SELECT category, name, price, FIRST_VALUE(name) " +
+            "OVER (ORDER BY price DESC " +
+            "ROWS BETWEEN UNBOUNDED PRECEDING " +
+            "AND UNBOUNDED FOLLOWING) " +
+            "AS den_prod_cu_pret_maxim " +
+            "FROM  (SELECT * " +
+            "FROM products_dim_part PARTITION (P_MADE_IN_ROMANIA) " +
+            "ORDER BY id) ", nativeQuery = true)
+    List<Object[]> mostExpensiveProductsRo();
+
+    @Query(value = "SELECT category, name, price, FIRST_VALUE(name) " +
+            "OVER (ORDER BY price DESC " +
+            "ROWS BETWEEN UNBOUNDED PRECEDING " +
+            "AND UNBOUNDED FOLLOWING) " +
+            "AS den_prod_cu_pret_maxim " +
+            "FROM  (SELECT * " +
+            "FROM products_dim_part PARTITION (P_MADE_IN_UE) " +
+            "ORDER BY id) ", nativeQuery = true)
+    List<Object[]> mostExpensiveProductsUe();
 }

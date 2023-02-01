21a004909efc9f5c74854a0b3209ece9db702ab9
Gioancă Raul <raul-dumitru.gioanca@my.fmi.unibuc.ro>
2023-02-01 16:54:47 +0200

    prima  statistica

---
+++ src/main/java/com/springmvc/SpringMVC/SpringDW/controllers/BillingsFactController.java
@@ -0,0 +1,29 @@
+package com.springmvc.SpringMVC.SpringDW.controllers;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.springmvc.SpringMVC.SpringDW.repositories.BillingsFactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class BillingsFactController {
+
+    @Autowired
+    private BillingsFactRepository billingsFactRepository;
+
+    @GetMapping("/products-month")
+    public String getBillingData(Model model) throws JsonProcessingException {
+        List<Object[]> data = billingsFactRepository.findTotalBillingPerMonth("2023");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String dataJson = objectMapper.writeValueAsString(data);
+        model.addAttribute("data", dataJson);
+
+        return "billing-chart1";
+    }
+}
--- src/main/java/com/springmvc/SpringMVC/SpringDW/repositories/BillingsFactRepository.java
+++ src/main/java/com/springmvc/SpringMVC/SpringDW/repositories/BillingsFactRepository.java
@@ -2,6 +2,19 @@ package com.springmvc.SpringMVC.SpringDW.repositories;
 
 import com.springmvc.SpringMVC.SpringDW.models.BillingsFact;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface BillingsFactRepository extends JpaRepository<BillingsFact, Integer> {
+
+
+    @Query(value = "SELECT time_dim.month, SUM(billings_fact.product_amount) as total_products " +
+            "FROM billings_fact " +
+            "JOIN time_dim ON billings_fact.time_id = time_dim.id " +
+            "WHERE time_dim.year = :year " +
+            "GROUP BY time_dim.month", nativeQuery = true)
+    List<Object[]> findTotalBillingPerMonth(@Param("year") String year);
+
 }
